39317bb509abd0a917fcaa45d4af201bbd0117f6
RaeesRizwan09 <96179641+RaeesRizwan09@users.noreply.github.com>
2024-09-10 19:53:49 +0500

    Update functions.h

--- module/src/main/cpp/functions.h
+++ module/src/main/cpp/functions.h
@@ -17,7 +17,7 @@ bool isGodMod(void *instance)
 }
 
 void Hooks() {
-    //IL2Cpp::Il2CppAttach();
+    IL2Cpp::Il2CppAttach();
 
     //Tools::Hook((void *) (uintptr_t)IL2Cpp::Il2CppGetMethodOffset(OBFUSCATE("Assembly-CSharp.dll"), OBFUSCATE(""), OBFUSCATE("PlayerStatus") , OBFUSCATE("get_IsInvincibility"), 0), (void *) isGodMod, (void **) &old_godmod);
 }
